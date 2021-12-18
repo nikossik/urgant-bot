91f584cac1ca433d5dd9aeea7c2d33c4d335cd8e
sexozavr <daniildiveev@yandex.ru>
2021-12-18 22:23:10 +0300

    created two functions and rbc parser

--- parsers.py
+++ parsers.py
@@ -2,9 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def create_url(url_start:str, query:str) -> str:
+    url = url_start
+
+    for word in query.split():
+        url += f"{word}+"
+
+    return url[:-1]
+
+def preprocess_text(corpus) -> list:
+    for i in range(len(corpus)):
+        corpus[i] = str(corpus[i]).replace('[', '').replace(']', '')
+        corpus[i] = re.sub(r"<.*?>", "", corpus[i])
+        corpus[i] = re.sub(r"\xa0|\n", " ", corpus[i])
+        corpus[i] = re.sub(r"\s+", " ", corpus[i])
+
+    return corpus
+
+
 def tatler_parser(query:str) -> list:
     url_parts = ['https://www.tatler.ru/search?q=','&sort=score+desc']
-    links, texts = [], []
+    links = []
 
     for word in query.split():
         url_parts[0] += f'{word}+'
@@ -29,22 +47,11 @@ def tatler_parser(query:str) -> list:
         text = str(div_el).replace('[', '').replace(']','')
         p_els = re.findall(r'<p>(.*?)</p>', text)
 
-        for i in range(len(p_els)):
-            p_els[i] = re.sub(r"<.*?>", "", p_els[i])
-            p_els[i] = re.sub(r"\xa0|\n", "", p_els[i])
-            p_els[i] = re.sub(r"\s+", " ", p_els[i])
-
-        texts += p_els
-    
-    return texts
+    return preprocess_text(p_els)
 
 def sobaka_parser(query:str) -> list:
-    url = 'https://www.sobaka.ru/search/all?q='
+    url = create_url('https://www.sobaka.ru/search/all?q=', query)
     links, texts = [], []
-
-    for word in query.split():
-        url += f"{word}+"
-    url = url[:-1]
 
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
@@ -60,23 +67,15 @@ def sobaka_parser(query:str) -> list:
 
         div_els = soup.find_all('div', {'class':'b-editors-text b-editors-text--lead'})
 
-        text = str(div_els).replace('[', '').replace(']', '')
-        text = re.sub(r"<.*?>", "", text)
-        text = re.sub(r"\xa0|\n", " ", text)
-        text = re.sub(r"\s+", " ", text)
+        preprocessed_text = preprocess_text(div_els)
 
-        if text != '': texts.append(text)
+        if len(preprocessed_text) != 0 : texts.append(preprocess_text(div_els)[0])
 
     return texts
 
 def esquire_parser(query:str) -> list:
-    url = "https://esquire.ru/search/?query="
+    url = create_url("https://esquire.ru/search/?query=", query)
     texts, links = [], []
-
-    for word in query.split():
-        url+= f'{word}+'
-    
-    url = url[:-1]
 
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
@@ -94,22 +93,14 @@ def esquire_parser(query:str) -> list:
         text = soup.find_all('div', {'class':'text-page'})
         p_els = re.findall(r'<p>(.*?)</p>', str(text))
 
-        for i in range(len(p_els)):
-            p_els[i] = re.sub(r"<.*?>", "", p_els[i])
-            p_els[i] = re.sub(r"\xa0|\n", " ", p_els[i])
-            p_els[i] = re.sub(r"\s+", " ", p_els[i])
-
-        texts += p_els
+        texts += preprocess_text(p_els)
 
     return texts
 
 def kommersant_parser(query:str) -> list:
-    url = 'https://www.kommersant.ru/search/results?search_query='
+    url = create_url('https://www.kommersant.ru/search/results?search_query=', query)
+    print(url)
     links, texts = [], []
-
-    for word in query.split():
-        url += f'{word}+'
-    url = url[:-1]
 
     html = requests.get(url).text
     soup = BeautifulSoup(html,"html.parser")
@@ -124,13 +115,30 @@ def kommersant_parser(query:str) -> list:
         soup = BeautifulSoup(html, "html.parser")
 
         p_els = soup.find_all('p', {'class':'doc__text'})
+        #print(p_els)
 
-        text = str(p_els).replace('[', '').replace(']', '')
-        text = re.sub(r"<.*?>", "", text)
-        text = re.sub(r"\xa0|\n", " ", text)
-        text = re.sub(r"\s+", " ", text)
-
-        texts.append(text)
+        texts.append(preprocess_text(p_els)[0])
 
     return texts
-kommersant_parser('сергей лавров')
+
+def rbc_parser(query:str) -> list:
+    url = create_url('https://www.rbc.ru/search/?project=rbcnews&query=', query)
+    links, texts = [], []
+    
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    a_els = soup.find_all('a', {'class':'search-item__link'})
+    
+    for a_el in a_els:
+        links.append(a_el['href'])
+
+    for link in links: 
+        html  = requests.get(link).text
+        soup = BeautifulSoup(html, "html.parser")
+
+        p_els = soup.find_all('p')
+
+        texts.append(preprocess_text(p_els)[0])
+
+    return texts
